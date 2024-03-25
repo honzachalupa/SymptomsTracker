a95759ad8a1b2a0741363b54aa79f2559cedd069
Jan Chalupa <janchalupa@outlook.cz>
2024-03-25 17:29:15 +0100

    WIP.

--- SymptomsTracker/Managers/Insights.swift
+++ SymptomsTracker/Managers/Insights.swift
@@ -9,55 +9,53 @@ struct ChatGPTResponseItem: Codable {
 class InsightsManager: ObservableObject {
     private let api = ChatGPTAPI(apiKey: "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")
     private var dataStore = DataStoreManager()
-    private var lastTimeRefreshed: Date = Date()
     
     @Published var advices: [ChatGPTResponseItem] = []
     @Published var isLoading: Bool = false
     
     init() {
-        // let oneHourElapsed = Calendar.current.date(byAdding: .hour, value: 1, to: lastTimeRefreshed)! < Date()
+        let oneHourElapsed = true // Calendar.current.date(byAdding: .hour, value: 1, to: lastTimeRefreshed)! > Date()
         
-        // if oneHourElapsed {
-            self.refreshInsights()
-        // }
-    }
-    
-    public func refreshInsights() {
-        isLoading = true
-        lastTimeRefreshed = Date()
-        
-        let message = getMessage(dataStore.symptoms, dataStore.triggers)
-        
-        consoleLog("CHAT_GPT message", message)
-        
-        Task {
-            do {
-                let response = try await api.sendMessage(text: message)
-                
-                consoleLog("CHAT_GPT response", response)
-                
-                let decoder = JSONDecoder()
-                let parsedResponse = try decoder.decode([ChatGPTResponseItem].self, from: response.data(using: .utf8)!)
-                
-                consoleLog("CHAT_GPT parsedResponse", parsedResponse)
-                
-                parsedResponse.forEach { advice in
-                    advices.append(advice)
-                }
-                
-                isLoading = false
-            } catch {
-                consoleLog("CHAT_GPT error", "\(error)")
-                
-                isLoading = false
+        if oneHourElapsed {
+            Task {
+                await self.refreshInsights()
             }
         }
     }
     
-    private func getMessage(_ symptoms: [Symptom], _ triggers: [Trigger]) -> String {
+    public func refreshInsights() async {
+        isLoading = true
+        
+        let message = getMessage()
+        
+        consoleLog("INSIGHTS message", message)
+        
+        do {
+            let response = try await api.sendMessage(text: message)
+            
+            consoleLog("INSIGHTS response", response)
+            
+            let decoder = JSONDecoder()
+            let parsedResponse = try decoder.decode([ChatGPTResponseItem].self, from: response.data(using: .utf8)!)
+            
+            parsedResponse.forEach { advice in
+                advices.append(advice)
+            }
+            
+            consoleLog("INSIGHTS cache set", parsedResponse)
+            
+            isLoading = false
+        } catch {
+            consoleLog("INSIGHTS error", "\(error)")
+            
+            isLoading = false
+        }
+    }
+    
+    private func getMessage() -> String {
         return """
-Input data: Symptoms: \(symptoms.map { $0.name }.joined(separator: ", "))
-Triggers: \(triggers.map { $0.name }.joined(separator: ", "))
+Input data: Symptoms: \(dataStore.symptoms.map { $0.name }.joined(separator: ", "))
+Triggers: \(dataStore.triggers.map { $0.name }.joined(separator: ", "))
 Response locale: \(Locale.preferredLanguages[0])
 
 I am suffering from health symptoms and they may be causing them.
--- SymptomsTracker/Views/InsightsView.swift
+++ SymptomsTracker/Views/InsightsView.swift
@@ -4,30 +4,40 @@ struct Insights: View {
     @ObservedObject var insightsManager = InsightsManager()
     
     var body: some View {
-        if insightsManager.isLoading {
-            HStack {
-                Spacer()
-                
-                ProgressView {
-                    Text("Creating AI insights...")
+        VStack {
+            Spacer()
+            
+            if insightsManager.isLoading {
+                HStack {
+                    Spacer()
+                    
+                    ProgressView {
+                        Text("Creating AI insights...")
+                    }
+                    
+                    Spacer()
                 }
-                
-                Spacer()
-            }
-        } else {
-            TabView {
-                ForEach(insightsManager.advices, id: \.content) { advice in
-                    VStack {
-                        Text(advice.content)
-                            .multilineTextAlignment(.center)
-                        
-                        Text("Relates with: " + advice.relations.joined(separator: ", "))
+            } else {
+                TabView {
+                    ForEach(insightsManager.advices, id: \.content) { advice in
+                        VStack {
+                            Text(advice.content)
+                                .multilineTextAlignment(.center)
+                            
+                            Text("Relates with: " + advice.relations.joined(separator: ", "))
+                                .opacity(0.5)
+                                .font(.footnote)
+                                .padding(.top, 3)
+                        }
                     }
                 }
+                .tabViewStyle(.page)
+                .indexViewStyle(.page(backgroundDisplayMode: .always))
             }
-            .tabViewStyle(.page)
-            .indexViewStyle(.page(backgroundDisplayMode: .always))
+            
+            Spacer()
         }
+        .frame(minHeight: 140)
     }
 }
 
